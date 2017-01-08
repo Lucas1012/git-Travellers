6a84296d3717534c102c1085faab8b505bcd08ad
Lucas <18410140018@163.com>
2017-01-08 15:46:06 +0800

    添加Host

--- app/src/main/java/com/mountain/travellers/MainActivity.java
+++ app/src/main/java/com/mountain/travellers/MainActivity.java
@@ -1,13 +1,120 @@
 package com.mountain.travellers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentTabHost mTabHost;
+    private ViewPager mViewPager;
+    private List<Fragment> mFragmentList;
+    private Class mClass[] = {ViewFragment.class, LocationFragment.class, CommonSenseFragment.class, MineFragment.class};
+    private Fragment mFragment[] = {new ViewFragment(), new LocationFragment(), new CommonSenseFragment(), new MineFragment()};
+    private String mTitles[] ;
+    private int mImages[] = {
+            R.drawable.tab_home,
+            R.drawable.tab_report,
+            R.drawable.tab_message,
+            R.drawable.tab_mine
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        init();
+
+    }
+
+    private void init() {
+
+        initView();
+
+        initEvent();
+    }
+
+    private void initView() {
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mTitles = getResources().getStringArray(R.array.tabs_title);
+        mFragmentList = new ArrayList<>();
+
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+        mTabHost.getTabWidget().setDividerDrawable(null);
+
+        for (int i = 0; i < mFragment.length; i++) {
+            FragmentTabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTitles[i]).setIndicator(getTabView(i));
+            mTabHost.addTab(tabSpec, mClass[i], null);
+            mFragmentList.add(mFragment[i]);
+            mTabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.WHITE);
+        }
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragmentList.size();
+            }
+        });
+    }
+
+    private View getTabView(int index) {
+        View view = LayoutInflater.from(this).inflate(R.layout.tab_item, null);
+
+        ImageView image = (ImageView) view.findViewById(R.id.image);
+        TextView title = (TextView) view.findViewById(R.id.title);
+
+        image.setImageResource(mImages[index]);
+        title.setText(mTitles[index]);
+
+        return view;
+    }
+
+    private void initEvent() {
+
+        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                mViewPager.setCurrentItem(mTabHost.getCurrentTab());
+            }
+        });
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mTabHost.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
